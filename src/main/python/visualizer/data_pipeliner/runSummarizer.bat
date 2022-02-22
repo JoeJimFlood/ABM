d960a882f61e8c063ba6fe80a13f23bf10925b15
SANDAGNET\jflo <jflo@sandag.org>
2022-02-22 14:55:20 -0800

    Added data pipeline tool to model flow

---
+++ src/main/python/visualizer/data_pipeliner/runSummarizer.bat
@@ -0,0 +1,17 @@
+:: Creating virtual Python environment
+set PYTHON_ENV=C:\ProgramData\Anaconda3
+call %PYTHON_ENV%\Scripts\activate.bat %PYTHON_ENV%
+call conda env list | find /i "viz"
+if not errorlevel 1 (
+	call conda env update --name viz --file %WORKING_DIR%environment.yml
+	call activate viz
+) else (
+	call conda env create -f %WORKING_DIR%environment.yml
+)
+call activate viz
+
+call python run.py
+
+call conda deactivate viz
+
+pause
